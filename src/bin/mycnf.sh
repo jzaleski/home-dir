484cb01f215745395799b6b79703343613ad4c48
Jonathan W. Zaleski <JonathanZaleski@gmail.com>
2012-11-27 22:45:01 -0500

    Add "generate" option to mycnf-helper

--- src/bin/mycnf.sh
+++ src/bin/mycnf.sh
@@ -1,12 +1,28 @@
 #!/bin/bash
 
-if [ $# -eq 1 ];
+if [ $# -gt 2 ] && [ $1 == 'generate' ];
 then
-	my_cnf_file="$HOME/.my.cnf.$1"
-	if [ -f $my_cnf_file ];
+	file=~/.my.cnf.$2;
+	echo -e "!include ~/.base.my.cnf\n\n[client]" > $file;
+	for arg in "${@:3}";
+	do
+		echo "${arg/--/}" >> $file;
+	done
+	exit 0;
+fi
+
+if [ $# -eq 1 ] && [ $1 != 'generate' ];
+then
+	file=~/.my.cnf.$1;
+	if [ -f $file ];
 	then
-		ln -sf $my_cnf_file ~/.my.cnf;
+		ln -sf $file ~/.my.cnf;
+		exit 0;
 	else
-		echo "could not find a .my.cnf file at: $my_cnf_file";
+		echo "could not find a .my.cnf file at: $file";
+		exit 255;
 	fi
 fi
+
+echo "Usage: `basename $0` [my_cnf_file] | [generate --key=value ... ]"
+exit 2;
