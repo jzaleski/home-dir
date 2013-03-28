e44613c65a44cf3f74d8b1358f7af1b8eb0f85a0
Jonathan W. Zaleski <JonathanZaleski@gmail.com>
2013-03-28 10:17:48 -0400

    Create "edit" script to launch default "TEXT_EDITOR"

---
+++ home/bin/edit.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Validate the text-editor is environment variable is specified
+if [ -z "$TEXT_EDITOR" ];
+then
+  echo 'No default text-editor specified (use: "TEXT_EDITOR" environment variable)';
+  exit 255;
+fi
+
+# Verify that the "open" command exists
+open_cmd=`which open`;
+if [ -z "$open_cmd" ];
+then
+  echo 'The "open" binary was not found in your "PATH"';
+  exit 255;
+fi
+
+# Always pass along all the arguments, this will allow for opening
+# single/multiple files as well as an empty editor instance
+$open_cmd -a $TEXT_EDITOR "$@";
