d18a4696ddcce4da4dd45cc462448c817360bb0b
Jonathan W. Zaleski <JonathanZaleski@gmail.com>
2013-12-16 08:18:30 -0500

    Build the "tags" file OOB and then move it into place

--- home/bin/ctags.sh
+++ home/bin/ctags.sh
@@ -18,6 +18,9 @@ gem ctags > /dev/null 2>&1 &
 # verify that the ctags library is present
 if hash ctags 2> /dev/null;
 then
-  rm -f tags;
-  ctags --tag-relative -Rf tags --exclude=.git > /dev/null 2>&1 &
+  # append a timestamp to the end of the "tags" file so that it can be built out
+  # of band and then moved into the expected location (this means that  the last
+  # process to finish will win)
+  tag_file="tags.`date +%s`";
+  ctags --tag-relative -Rf $tag_file --exclude=.git > /dev/null 2>&1 && mv $tag_file tags &
 fi
