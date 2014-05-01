e6c0d786e0f4627560807a861688e78f84901587
Jonathan W. Zaleski <JonathanZaleski@gmail.com>
2014-05-01 18:04:06 -0400

    Install additional base-packages via "apt"
    Also, fetch and compile "ag" (the silver searcher) from source

--- sudo_script/apt_get_install_base_packages.sh
+++ sudo_script/apt_get_install_base_packages.sh
@@ -2,9 +2,34 @@
 
 if [ -n "$EXTENDED_BOOTSTRAP" ];
 then
+  # install apt packages
   apt_get_cmd=`which apt-get`;
   if [ -n "$apt_get_cmd" ];
   then
-    $apt_get_cmd install -y apg exuberant-ctags git tmux vim zsh;
+    $apt_get_cmd install -y \
+      apg \
+      bash-completion \
+      build-essential \
+      exuberant-ctags \
+      git \
+      htop \
+      tmux \
+      vim \
+      wget \
+      zsh;
+  fi
+  # install "ag" (if it isn't already present)
+  ag_cmd=`which ag`;
+  if [ -z "$ag_cmd" ];
+  then
+    (
+      cd /tmp &&
+      git clone git://github.com/ggreer/the_silver_searcher.git
+      cd the_silver_searcher &&
+      sh build.sh &&
+      make install &&
+      cd .. &&
+      rm -rf the_silver_searcher;
+    );
   fi
 fi
