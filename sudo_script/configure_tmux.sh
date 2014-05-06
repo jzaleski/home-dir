b0e76018022cdf36ffba368f47b5f79339e52a8f
Jonathan W. Zaleski <JonathanZaleski@gmail.com>
2014-05-06 15:11:01 -0400

    Create script to manually build/install "tmux"

---
+++ sudo_script/configure_tmux.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ -n "$EXTENDED_BOOTSTRAP" ] && [ -z `which tmux` ];
+then
+  apt_cmd=`which apt-get`;
+  if [ -n "$apt_cmd" ];
+  then
+    $apt_cmd install -y libncurses5-dev libevent-dev &&
+    cd /tmp &&
+    wget http://downloads.sourceforge.net/tmux/tmux-1.9a.tar.gz &&
+    tar -xvzf tmux-1.9a.tar.gz &&
+    cd tmux-1.9a &&
+    ./configure &&
+    make &&
+    make install &&
+    cd .. &&
+    rm -rf tmux-1.9a;
+  fi
+fi
