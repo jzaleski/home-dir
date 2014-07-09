0774c62181b718108c271d9bb2643414e6a55008
Jonathan W. Zaleski (WorldOne.com) <JonathanZaleski@gmail.com>
2014-07-09 09:58:37 -0400

    Add script to execute a command w/ all RVM rubies
    * Create "with_rvm_rubies.sh" script
    * Wrap script in "with_rvm_rubies" alias

---
+++ home/bin/with_rvm_rubies.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+if [ $# -eq 0 ]; then
+  echo "Usage: `basename $0` [command]";
+  exit 2;
+fi
+
+rvm_cmd=`which rvm`;
+if [ -z "$rvm_cmd" ]; then
+  echo "Could not locate an \"rvm\" executable";
+  exit 1;
+fi
+
+ruby_versions=`$rvm_cmd list | \grep -oE "(jruby|rbx|ruby)-[^ ]+"`;
+if [ -z "$ruby_versions" ]; then
+  echo "You must install at least one version of ruby to use this script";
+  exit 1;
+fi
+
+for ruby_version in $ruby_versions; do
+  bash -l -c "\
+    rvm $ruby_version@`basename $(pwd)` --create &&
+    [[ -z `which bundle` ]] && gem install bundler || true &&
+    bundle &&
+    $@;
+  ";
+done
