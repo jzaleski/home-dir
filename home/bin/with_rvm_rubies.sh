1229531bfbeb225e48362e5fcc0d73eac7ad29ba
Jonathan W. Zaleski (WorldOne.com) <JonathanZaleski@gmail.com>
2014-07-09 13:07:11 -0400

    A little more clean-up for "with_rvm_rubies.sh"
    * If "rvm" is not zero-length (nil) assume the script is in the path and
      that the "rvm" function is loaded

--- home/bin/with_rvm_rubies.sh
+++ home/bin/with_rvm_rubies.sh
@@ -5,18 +5,17 @@ if [ $# -eq 0 ]; then
   exit 2;
 fi
 
-rvm_cmd=`which rvm`;
-if [ -z "$rvm_cmd" ]; then
-  echo "Could not locate an \"rvm\" executable";
+if [ -z `which rvm` ]; then
+  echo "Could not locate \"rvm\"";
   exit 1;
 fi
 
-ruby_versions=`$rvm_cmd list | \grep -oE "(jruby|rbx|ruby)-[^ ]+"`;
+ruby_versions=`rvm list | \grep -oE "(jruby|rbx|ruby)-[^ ]+"`;
 if [ -z "$ruby_versions" ]; then
   echo "You must install at least one version of ruby to use this script";
   exit 1;
 fi
 
 for ruby_version in $ruby_versions; do
-  bash -l -c "$rvm_cmd $ruby_version@`basename $(pwd)` --create && $@";
+  bash -l -c "rvm $ruby_version@`basename $(pwd)` --create && $@";
 done
