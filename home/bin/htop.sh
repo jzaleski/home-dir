35d4dabca6350023ae68ae4022341825f225cdc6
Jonathan W. Zaleski <JonathanZaleski@gmail.com>
2014-10-20 09:03:13 -0400

    Create wrapper script for htop
    On OSX (Darwin), htop must be started with sudo privileges in order to
    see actual system usage (and be able to administer privileged processes)

---
+++ home/bin/htop.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+htop_cmd=`which htop`
+if [ -z "$htop_cmd" ]; then
+  echo "Could not locate the \"htop\" binary";
+  exit 1;
+fi
+
+if [ `uname` = 'Darwin' ]; then
+  sudo $htop_cmd;
+else
+  $htop_cmd;
+fi
