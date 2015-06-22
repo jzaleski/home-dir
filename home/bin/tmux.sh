66043bc1fc178fafb257f70c81e6113ac2150298
Jonathan W. Zaleski <JonathanZaleski@gmail.com>
2015-06-22 17:03:08 -0400

    Remove 'tmux' helper-script

--- home/bin/tmux.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-tmux_cmd=`\which tmux 2> /dev/null`;
-if [ -z "$tmux_cmd" ]; then
-  echo "Could not locate the \"tmux\" binary";
-  exit 1;
-fi
-
-($tmux_cmd attach || $tmux_cmd) 2> /dev/null;
