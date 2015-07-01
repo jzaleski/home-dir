eadaafd557cf8d0e56f785a73788e74c193e860e
Jonathan W. Zaleski <JonathanZaleski@gmail.com>
2015-07-01 15:34:46 -0400

    Ensure only one instance of ctags is running at a given time + clean-up

--- home/bin/ctags.sh
+++ home/bin/ctags.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 
-# attempt to install the homebrew ctags library (if necessary)
+# ensure that the pkill binary is available
+if ! hash pkill 2> /dev/null; then
+  echo "Could not locate the \"pkill\" binary";
+  exit 1;
+fi
+
+# attempt to install the homebrew ctags binary (if necessary)
 if hash brew 2> /dev/null && ! brew --prefix ctags > /dev/null 2>&1; then
   brew install ctags;
 fi
@@ -8,16 +14,27 @@ fi
 # attempt to install "gem-ctags" (if necessary)
 if hash rvm 2> /dev/null && ! gem list | \grep 'gem-ctags' > /dev/null 2>&1; then
   bash -l -c "rvm gemset use global && gem install gem-ctags";
+elif ! gem list | \grep 'gem-ctags' > /dev/null 2>&1; then
+  gem install gem-ctags;
 fi
 
-# generate ctags for gems
-gem ctags > /dev/null 2>&1 &
+# generate ctags for gems (if necessary)
+if gem list | \grep 'gem-ctags' > /dev/null 2>&1; then
+  gem ctags > /dev/null 2>&1 &
+fi
 
-# verify that the ctags library is present
+# ensure that only one ctags instance is running for a given project (if there
+# is already a process running, kill it and clean up after it (it won't do so
+# itself)
+lock_file=tags.lock;
+if [ -f $lock_file ]; then
+  pkill -P `cat $lock_file`;
+  rm -f tags.*;
+fi
+
+# verify that the ctags binary is present
 if hash ctags 2> /dev/null; then
-  # append a timestamp to the end of the "tags" file so that it can be built out
-  # of band and then moved into the expected location (this means that  the last
-  # process to finish will win)
-  tag_file="tags.`date +%s`";
-  ctags --tag-relative -Rf $tag_file --exclude=.git > /dev/null 2>&1 && mv $tag_file tags &
+  tags_file="tags.`date +%s`";
+  ctags --tag-relative -Rf $tags_file --exclude=.git > /dev/null 2>&1 && mv $tags_file tags && rm -f $lock_file &
+  echo $! > $lock_file;
 fi
