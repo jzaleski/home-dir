92e3545829652bef17376e72d4f31b4fb96f4d2a
Jonathan W. Zaleski <JonathanZaleski@gmail.com>
2015-07-06 16:46:45 -0400

    ctags generation tweaks
    Create a tags-file with a ".tmp" extension as opposed to a tags-file
    with the current timestamp as its extension (this was making the file
    hard to ignore)

--- home/bin/ctags.sh
+++ home/bin/ctags.sh
@@ -11,6 +11,12 @@ if hash brew 2> /dev/null && ! brew --prefix ctags > /dev/null 2>&1; then
   brew install ctags;
 fi
 
+# ensure that the ctags binary is available
+if ! hash ctags 2> /dev/null; then
+  echo "Could not locate the \"ctags\" binary";
+  exit 1;
+fi
+
 # attempt to install "gem-ctags" (if necessary)
 if hash rvm 2> /dev/null && ! gem list | \grep 'gem-ctags' > /dev/null 2>&1; then
   bash -l -c "rvm gemset use global && gem install gem-ctags";
@@ -23,18 +29,21 @@ if gem list | \grep 'gem-ctags' > /dev/null 2>&1; then
   gem ctags > /dev/null 2>&1 &
 fi
 
+# execution artifact(s)
+lock_file=tags.lock;
+temp_file=tags.tmp;
+
 # ensure that only one ctags instance is running for a given project (if there
 # is already a process running, kill it and clean up after it (it won't do so
 # itself)
-lock_file=tags.lock;
 if [ -f $lock_file ]; then
   pkill -P `cat $lock_file`;
-  rm -f tags.*;
+  rm -f $lock_file $temp_file;
 fi
 
-# verify that the ctags binary is present
-if hash ctags 2> /dev/null; then
-  tags_file="tags.`date +%s`";
-  ctags --tag-relative -Rf $tags_file --exclude=.git > /dev/null 2>&1 && mv $tags_file tags && rm -f $lock_file &
-  echo $! > $lock_file;
-fi
+# start and background the [c]tags generation process
+ctags --tag-relative -Rf $temp_file --exclude=.git > /dev/null 2>&1 && mv $temp_file tags && rm -f $lock_file &
+
+# capture the pid of the last background[ed] process (this will be used if this
+# script if re-invoked to determine which process to kill)
+echo $! > $lock_file;
