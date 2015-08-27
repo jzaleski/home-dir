5978020228bbfd4c915cad75394cc27a74c62f09
Jonathan W. Zaleski <JonathanZaleski@gmail.com>
2015-08-27 12:38:22 +0000

    Symlink Downloads -> tmp

--- script/configure_tmp.sh
+++ script/configure_tmp.sh
@@ -1,4 +1,20 @@
 #!/usr/bin/env bash
 
-# ensure that the [user-specific] temporary directory exists
+# ensure that the [user-specific] downloads directory exists
 mkdir -p $HOME/tmp;
+downloads_dir="$HOME/Downloads";
+if [ ! -d "$downloads_dir" ]; then
+  mkdir $downloads_dir;
+fi
+
+# if $temp_dir exists, relocate its contents and remove it
+temp_dir=$HOME/tmp;
+if [ -d "$temp_dir" ]; then
+  mv $temp_dir/* $downloads_dir;
+  rm -r $temp_dir;
+fi
+
+# Symlink $temp_dir to $downloads_dir
+if [ ! -h "$temp_dir" ]; then
+  ln -s $downloads_dir $temp_dir;
+fi
