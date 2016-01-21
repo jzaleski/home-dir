46af782e77a7f325ab4af3e7a04553010784fd7a
Jonathan W. Zaleski <JonathanZaleski@gmail.com>
2016-01-21 14:38:53 +0000

    Tweaks to `ag` configuration script plus add `autocutsel` configuration script

--- sudo_script/configure_ag.sh
+++ sudo_script/configure_ag.sh
@@ -1,25 +1,11 @@
 #!/usr/bin/env bash
 
 if [ -n "$EXTENDED_BOOTSTRAP" ] && [ ! -d /tmp/the_silver_searcher ]; then
-  apt_get_cmd=`\which apt-get 2> /dev/null`;
-  if [ -n "$apt_get_cmd" ]; then
-    $apt_get_cmd install -y \
-      liblzma5 \
-      liblzma-dev \
-      libpcre3 \
-      libpcre3-dev;
-  fi
-
   yum_cmd=`\which yum 2> /dev/null`;
   if [ -n "$yum_cmd" ]; then
     $yum_cmd install -y \
       pcre-devel \
       xz-devel;
-  fi
-
-  if [ -z "$apt_get_cmd" ] && [ -z "$yum_cmd" ]; then
-    echo "Could not configure \"ag\"";
-    exit 1;
   fi
 
   cd /tmp &&
