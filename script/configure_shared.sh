6e601daed777e2982748f43e8a7ee8eb45f59775
Jonathan W. Zaleski <JonathanZaleski@gmail.com>
2016-03-11 16:22:30 -0500

    Also add a `Shared` sym-link to the Desktop

--- script/configure_shared.sh
+++ script/configure_shared.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+desktop_dir=$HOME/Desktop;
+desktop_shared_dir=$desktop_dir/Shared;
 home_shared_dir=$HOME/Shared;
 mnt_shared_dir=/mnt/Shared;
 if [ -d $mnt_shared_dir ] && [ ! -h $home_shared_dir ]; then
@@ -8,4 +10,8 @@ fi
 
 if [ ! -h $home_shared_dir ] && [ ! -d $mnt_shared_dir ]; then
   mkdir -p $home_shared_dir;
+fi
+
+if [ -d $desktop_dir ] && [ ! -h $desktop_shared_dir ]; then
+  ln -s $home_shared_dir $desktop_shared_dir;
 fi
