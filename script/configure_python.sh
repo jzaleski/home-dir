03110d769ef61a0bc62a6b35523fb69c03fea6cf
Jonathan W. Zaleski <JonathanZaleski@gmail.com>
2016-03-15 22:39:50 -0400

    Simplify python configuration scripts

--- script/configure_python.sh
+++ script/configure_python.sh
@@ -3,61 +3,9 @@
 if [ -n "$EXTENDED_BOOTSTRAP" ]; then
   brew_cmd=`\which brew 2> /dev/null`;
   if [ -n "$brew_cmd" ]; then
-    # install the required system-level packages
-    if [ -z `\which python 2> /dev/null` ]; then
+    python_package_details=`$brew_cmd list python 2> /dev/null`;
+    if [ -z "$python_package_details" ]; then
       $brew_cmd install python;
-    fi
-
-    # if easy_install is not present we can't proceed
-    easy_install_cmd=`\which easy_install 2> /dev/null`;
-    if [ -z $easy_install_cmd ]; then
-      echo "Could not locate the \"easy_install\" binary";
-      exit 1;
-    fi
-
-    # install pip if it isn't already installed
-    if [ -z `\which pip 2> /dev/null` ]; then
-      $easy_install_cmd pip;
-    fi
-
-    # verify the pip installation
-    pip_cmd=`\which pip 2> /dev/null`;
-    if [ -z "$pip_cmd" ]; then
-      echo "Could not install \"pip\"";
-      exit 255;
-    fi
-
-    # install virtualenv if it isn't already installed
-    if [ -z `\which virtualenv 2> /dev/null` ]; then
-      $pip_cmd install virtualenv;
-    fi
-
-    # verify the virtualenv installation
-    if [ -z `\which virtualenv 2> /dev/null` ]; then
-      echo "Could not install \"virtualenv\"";
-      exit 255;
-    fi
-
-    # install ipython if it isn't already installed
-    if [ -z `\which ipython 2> /dev/null` ]; then
-      $pip_cmd install ipython;
-    fi
-
-    # verify the ipython installation
-    if [ -z `\which ipython 2> /dev/null` ]; then
-      echo "Could not install \"ipython\"";
-      exit 255;
-    fi
-
-    # install readline if it isn't already installed
-    if [ `$pip_cmd freeze | \grep -c "[r]eadline"` -eq 0 ]; then
-      $pip_cmd install readline;
-    fi
-
-    # verify the readline installation
-    if [ `$pip_cmd freeze | \grep -c "[r]eadline"` -eq 0 ]; then
-      echo "Could not install \"readline\"";
-      exit 255;
     fi
   fi
 fi
--- sudo_script/configure_python.sh
+++ sudo_script/configure_python.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-if [ -n "$EXTENDED_BOOTSTRAP" ] && [ `uname` != "Darwin" ]; then
+if [ -n "$EXTENDED_BOOTSTRAP" ]; then
   apt_get_cmd=`\which apt-get 2> /dev/null`;
   if [ -n "$apt_get_cmd" ]; then
     $apt_get_cmd install -y \
@@ -20,35 +20,5 @@ if [ -n "$EXTENDED_BOOTSTRAP" ] && [ `uname` != "Darwin" ]; then
       python-pip \
       python-setuptools \
       python-virtualenv;
-  fi
-
-  if [ -z "$apt_get_cmd" ] && [ -z "$yum_cmd" ]; then
-    echo "Could not configure \"python\"";
-    exit 1;
-  fi
-
-  # ensure that pip is available
-  pip_cmd=`\which pip 2> /dev/null`;
-  if [ -z "$pip_cmd" ]; then
-    echo "Could not locate the \"pip\" binary";
-    exit 1;
-  fi
-
-  # ensure that virtualenv is available
-  virtualenv_cmd=`\which virtualenv 2> /dev/null`;
-  if [ -z "$virtualenv_cmd" ]; then
-    echo "Could not locate the \"virtualenv\" binary";
-    exit 1;
-  fi
-
-  # install ipython if it isn't already installed
-  if [ -z `\which ipython 2> /dev/null` ]; then
-    $pip_cmd install ipython;
-  fi
-
-  # verify the ipython installation
-  if [ -z `\which ipython 2> /dev/null` ]; then
-    echo "Could not install \"ipython\"";
-    exit 255;
   fi
 fi
