c8bcb4f6b7dbb3df9607671cc6aa8f56a93f9a13
Jonathan W. Zaleski <JonathanZaleski@gmail.com>
2016-03-17 14:57:25 -0400

    Clean up neovim init-script

--- script/configure_neovim.sh
+++ script/configure_neovim.sh
@@ -7,5 +7,20 @@ if [ -n "$EXTENDED_BOOTSTRAP" ]; then
     if [ -z "$neovim_package_details" ]; then
       $brew_cmd install neovim/neovim/neovim;
     fi
+
+    config_dir=$HOME/.config;
+    if [ ! -d $config_dir ]; then
+      mkdir -p $config_dir;
+    fi
+
+    nvim_dir_link=$config_dir/nvim;
+    if [ ! -h $nvim_dir_link ]; then
+      ln -s $HOME/.vim $nvim_dir_link;
+    fi
+
+    init_dot_vim_link=$nvim_dir_link/.init.vim;
+    if [ ! -h $init_dot_vim_file_link ]; then
+      ln -s $HOME/.vimrc $init_dot_vim_file_link;
+    fi
   fi
 fi
