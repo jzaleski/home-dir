afb8f067d4a2bd4ea790d5e3b777919bb64e80a5
Jonathan W. Zaleski <JonathanZaleski@gmail.com>
2016-03-17 14:19:52 -0400

    Use brew to determine if the correct version of ag is installed

--- script/configure_ag.sh
+++ script/configure_ag.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
-if [ -n "$EXTENDED_BOOTSTRAP" ] && [ `uname` = "Darwin" ]; then
+if [ -n "$EXTENDED_BOOTSTRAP" ]; then
   brew_cmd=`\which brew 2> /dev/null`;
   if [ -n "$brew_cmd" ]; then
-    if [ -z `\which ag 2> /dev/null` ]; then
+    ag_package_details=`$brew_cmd list ag 2> /dev/null`;
+    if [ -z "$ag_package_details" ]; then
       $brew_cmd install ag;
     fi
   fi
