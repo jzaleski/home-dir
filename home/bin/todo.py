16c75b2dd2b2a8279726cbe5322a5e7d1bb3dc09
Jonathan W. Zaleski <JonathanZaleski@gmail.com>
2017-10-09 09:56:21 -0400

    Clean-up todo.py

--- home/bin/todo.py
+++ home/bin/todo.py
@@ -6,15 +6,22 @@ import os, re, sys, time, uuid
 
 class Processor(object):
     ADDED_BUCKET = 'a'
+    ADD_OPERATIONS = {'a', 'add'}
+    BUCKET_PATTERN = r'^(a|added|d|done|r|removed)$'
     DEFAULT_LIST_NAME = 'TODO'
     DEFAULT_PARENT_ID = uuid.UUID('00000000-0000-0000-0000-000000000000')
     DONE_BUCKET = 'd'
+    DONE_OPERATIONS = {'d', 'done'}
+    EDIT_OPERATIONS = {'e', 'edit'}
+    INDEX_PATTERN = r'^[0-9]+$'
+    OPERATION_PATTERN = r'^(a|add|d|done|r|remove)$'
     REMOVED_BUCKET = 'r'
-
-    @property
-    def added(self):
-        return (datum for datum in self.database \
-            if datum.get('bucket') == self.ADDED_BUCKET)
+    REMOVE_OPERATIONS = {'r', 'remove'}
+    VALID_BUCKETS = {
+        ADDED_BUCKET,
+        DONE_BUCKET,
+        REMOVED_BUCKET,
+    }
 
     @property
     def database(self):
@@ -29,20 +36,10 @@ class Processor(object):
         return self._database_file_path
 
     @property
-    def done(self):
-        return (datum for datum in self.database \
-            if datum.get('bucket') == self.DONE_BUCKET)
-
-    @property
     def list_name(self):
         if not hasattr(self, '_list_name'):
             self._list_name = self._get_list_name()
         return self._list_name
-
-    @property
-    def removed(self):
-        return (datum for datum in self.database \
-            if datum.get('bucket') == self.REMOVED_BUCKET)
 
     @property
     def timestamp(self):
@@ -50,29 +47,29 @@ class Processor(object):
             self._timestamp = int(time.time())
         return self._timestamp
 
-    @property
-    def valid_buckets(self):
-        if not hasattr(self, '_valid_buckets'):
-            self._valid_buckets = self._get_valid_buckets()
-        return self._valid_buckets
-
     def process(self, *args):
         self._ensure_database_exists()
         if not args:
-            return self._render()
-        index = self._parse_index(args[0])
-        if index is None:
+            return self._render(self.ADDED_BUCKET)
+        if re.match(self.BUCKET_PATTERN, args[0]):
+            return self._render(args[0][0])
+        if not re.match(self.INDEX_PATTERN, args[0]):
             return self._add(message=' '.join(args))
+        index = int(args[0]) - 1
+        if index < 0:
+            return False
         if not args[1:]:
-            return self._render(index)
+            return False
+        if not re.match(self.OPERATION_PATTERN, args[1]):
+            return False
         operation = args[1]
-        if operation in ['a', 'add']:
+        if operation in self.ADD_OPERATIONS:
             return self._add(message=' '.join(args[1:]))
-        elif operation in ['d', 'done']:
+        elif operation in self.DONE_OPERATIONS:
             return self._done(index)
-        elif operation in ['e', 'edit']:
+        elif operation in self.EDIT_OPERATIONS:
             return self._edit(index, ' '.join(args[2:]))
-        elif operation in ['r', 'remove']:
+        elif operation in self.REMOVE_OPERATIONS:
             return self._remove(index)
         return False
 
@@ -95,15 +92,15 @@ class Processor(object):
         return self._write_database()
 
     def _done(self, index=None):
-        if not self.database:
+        bucket = self._get_bucket(self.ADDED_BUCKET)
+        if not bucket:
             return False
-        if index is None or index < 0:
-            return False
-        for datum_index, datum in enumerate(self.added):
-            if datum_index == index:
-                datum['updated_timestamp'] = self.timestamp
-                datum['bucket'] = self.DONE_BUCKET
-                return self._write_database()
+        for datum_index, datum in enumerate(bucket):
+            if datum_index != index:
+                continue
+            datum['updated_timestamp'] = self.timestamp
+            datum['bucket'] = self.DONE_BUCKET
+            return self._write_database()
         return False
 
     def _edit(
@@ -111,18 +108,17 @@ class Processor(object):
         index=None,
         message=None
     ):
-        if not self.database:
+        if not message:
             return False
-        if \
-            index is None or \
-            index < 0 or \
-            not message:
+        bucket = self._get_bucket(self.ADDED_BUCKET)
+        if not bucket:
             return False
-        for datum_index, datum in enumerate(self.added):
-            if datum_index == index:
-                datum['updated_timestamp'] = self.timestamp
-                datum['message'] = message
-                return self._write_database()
+        for datum_index, datum in enumerate(bucket):
+            if datum_index != index:
+                continue
+            datum['updated_timestamp'] = self.timestamp
+            datum['message'] = message
+            return self._write_database()
         return False
 
     def _ensure_database_exists(self):
@@ -132,6 +128,13 @@ class Processor(object):
             os.makedirs(database_dirname)
         if not os.path.isfile(database_file_path):
             open(self.database_file_path, 'w').close()
+
+    def _get_bucket(self, bucket):
+        return (
+            datum
+            for datum in self.database
+            if datum['bucket'] == bucket
+        )
 
     def _get_database(self):
         database = []
@@ -153,7 +156,7 @@ class Processor(object):
                 if not updated_timestamp:
                     continue
                 bucket = line_parts[4]
-                if bucket not in self.valid_buckets:
+                if bucket not in self.VALID_BUCKETS:
                     continue
                 message = line_parts[5]
                 if not message:
@@ -186,45 +189,26 @@ class Processor(object):
     def _get_list_name(self):
         return os.getenv('LIST_NAME', self.DEFAULT_LIST_NAME)
 
-    def _get_valid_buckets(self):
-        return {
-            self.ADDED_BUCKET,
-            self.DONE_BUCKET,
-            self.REMOVED_BUCKET,
-        }
-
-    def _parse_index(self, index):
-        if not re.match(r'^[0-9]+(:[0-9]+)*$', index):
-            return None
-        # TODO: Add support complex indices. For now the code-block below
-        # short-circuits with the first part of what is parsed -- changing
-        # `next` to `list` or to a list-comprehension will work/parse properly,
-        # but will cause breakage upstream
-        return next(int(index_part) - 1 for index_part in index.split(':'))
-
     def _remove(self, index=None):
-        if not self.database:
+        bucket = self._get_bucket(self.ADDED_BUCKET)
+        if not bucket:
             return False
-        if index is None or index < 0:
-            return False
-        for datum_index, datum in enumerate(self.added):
-            if datum_index == index:
-                datum['updated_timestamp'] = self.timestamp
-                datum['bucket'] = self.REMOVED_BUCKET
-                return self._write_database()
+        for datum_index, datum in enumerate(bucket):
+            if datum_index != index:
+                continue
+            datum['updated_timestamp'] = self.timestamp
+            datum['bucket'] = self.REMOVED_BUCKET
+            return self._write_database()
         return False
 
-    def _render(self, index=None):
-        if not self.database:
+    def _render(self, bucket):
+        bucket = self._get_bucket(bucket)
+        if not bucket:
             return False
-        if index is not None and index < 0:
-            return False
-        for datum_index, datum in enumerate(self.added):
-            if index is None or datum_index == index:
-                print('%3d. %s' % (datum_index + 1, datum['message']))
-            if datum_index == index:
-                return True
-        return index is None
+        index = 0
+        for datum_index, datum in enumerate(bucket):
+            print('%3d. %s' % (datum_index + 1, datum['message']))
+        return True
 
     def _write_database(self):
         with open(self.database_file_path, 'w') as database_file:
