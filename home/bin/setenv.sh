d88dae4ee0cebbcbf6bc0ad0e1182de05af9b24b
Jonathan W. Zaleski <JonathanZaleski@gmail.com>
2018-06-03 08:12:08 -0400

    Add `elixir` and `sbt` support to: `setenv`

--- home/bin/setenv.sh
+++ home/bin/setenv.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 if [ -n "$TYPE" ]; then
   type=$TYPE;
@@ -6,12 +6,17 @@ elif [ $# -ge 1 ]; then
   type=$1;
 fi
 
-if [[ $type =~ ^node ]]; then
+
+if [[ $type =~ ^elixir ]]; then
+  echo $type > $PWD/.exenv-version;
+elif [[ $type =~ ^node ]]; then
   echo $type > $PWD/.node-version;
 elif [[ $type =~ ^python ]]; then
   echo $type > $PWD/.python-version;
 elif [[ $type =~ ^[j]?ruby ]]; then
   echo $type > $PWD/.ruby-version;
+elif [[ $type =~ ^sbt ]]; then
+  echo $type > $PWD/.sbt-version;
 else
   echo "Invalid environment-type: \"$type\"";
 fi
