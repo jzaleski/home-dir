bf9dc295c8faf446b83b4133dbf72c25714ecdea
Jonathan W. Zaleski <JonathanZaleski@gmail.com>
2018-07-08 16:43:07 -0400

    `github` and `jira` helper improvements
    * Respect a local .jira-url file if present
    * When inside of a git-repo perform all operations relative to the
      origin-url
    * Attempt to use `xdg-open` if `open` is not present -- this *should*
      cover _most_ Linux distributions

--- home/bin/github.sh
+++ home/bin/github.sh
@@ -2,22 +2,43 @@
 
 open_cmd=`\which open 2> /dev/null`;
 if [ -z "$open_cmd" ]; then
-  echo "Could not locate the \"open\" binary";
+  open_cmd=`\which xdg-open 2> /dev/null`;
+fi
+
+if [ -z "$open_cmd" ]; then
+  echo "Could not locate the \"[xdg-]open\" binary";
   exit 1;
 fi
 
-github_url=${GITHUB_URL-https://www.github.com};
+github_url=$GITHUB_URL;
 
-argv0=$1;
-if [ -n "$argv0" ]; then
-  github_url="$github_url/$argv0";
+git_cmd=`\which git 2> /dev/null`;
+if [ -z "$github_url" ] && [ -n "$git_cmd" ]; then
+  if [ -e .git ]; then
+    github_url=`$git_cmd config --get remote.origin.url 2> /dev/null`;
+  fi
 fi
 
-argv1=$2;
-if [[ "$argv1" =~ ^[0-9]+$ ]]; then
-  github_url="$github_url/pull/$argv1";
-elif [ -n "$argv1" ]; then
-  github_url="$github_url/tree/$argv1";
+ssh_prefix="git@github.com:";
+if [[ "$github_url" =~ "$ssh_prefix" ]]; then
+  github_url=`\echo $github_url | \sed "s/$ssh_prefix//"`;
+fi
+
+dot_git_suffix=".git";
+if [[ "$github_url" =~ "$dot_git_suffix" ]]; then
+  github_url=`\echo $github_url | \sed "s/$dot_git_suffix\$//"`;
+fi
+
+https_prefix="https://github.com";
+if [[ ! "$github_url" =~ "$https_prefix" ]]; then
+  github_url="$https_prefix/$github_url";
+fi
+
+argv0=$1;
+if [[ "$argv0" =~ ^[0-9]+$ ]]; then
+  github_url="$github_url/pull/$argv0";
+elif [ -n "$argv0" ]; then
+  github_url="$github_url/tree/$argv0";
 fi
 
 $open_cmd "$github_url";
--- home/bin/jira.sh
+++ home/bin/jira.sh
@@ -2,15 +2,26 @@
 
 open_cmd=`\which open 2> /dev/null`;
 if [ -z "$open_cmd" ]; then
+  open_cmd=`\which xdg-open 2> /dev/null`;
+fi
+
+if [ -z "$open_cmd" ]; then
   echo "Could not locate the \"open\" binary";
   exit 1;
 fi
 
 jira_url=$JIRA_URL;
-jira_url_file=$HOME/.jira-url;
+if [ -z "$jira_url" ]; then
+  if [ -e .jira-url ]; then
+    jira_url=`\cat .jira-url`;
+  elif [ -e $HOME/.jira-url ]; then
+    jira_url=`\cat $HOME/.jira-url`;
+  fi
+fi
 
-if [ -z "$jira_url" ] && [ -e $jira_url_file ]; then
-  jira_url=`\cat $jira_url_file`;
+if [ -z "$jira_url" ]; then
+  echo "Could not determine Jira URL";
+  exit 1;
 fi
 
 argv0=$1;
