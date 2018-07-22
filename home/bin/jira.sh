4eb967005f906c6c16cc73ce9b4beee59a8336f3
Jonathan W. Zaleski <JonathanZaleski@gmail.com>
2018-07-22 10:17:33 -0400

    Use the branch-name as the issue-id if it matches the correct pattern for Jira

--- home/bin/jira.sh
+++ home/bin/jira.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
+set -e;
+
+git_cmd=`\which git 2> /dev/null`;
+if [ -z "$git_cmd" ]; then
+  echo "Could not locate the \"git\" binary";
+  exit 1;
+fi
+
 open_cmd=`\which open 2> /dev/null`;
 if [ -z "$open_cmd" ]; then
   open_cmd=`\which xdg-open 2> /dev/null`;
@@ -24,13 +32,30 @@ if [ -z "$jira_url" ]; then
   exit 1;
 fi
 
-argv0=$1;
-if [ "$argv0" = "dashboard" ]; then
-  jira_url="$jira_url/secure/Dashboard.jspa";
-elif [ -n "$argv0" ]; then
-  jira_url="$jira_url/browse/$argv0";
-else
+action_or_issue_id=$1;
+
+git_branch=`$git_cmd rev-parse --abbrev-ref HEAD 2> /dev/null`;
+if \
+  [ -z "$action_or_issue_id" ] && \
+  [ -n "$git_branch" ] && \
+  [[ "$git_branch" =~ ^[A-Z]+-[0-9]+$ ]];
+then
+  action_or_issue_id=$git_branch;
+fi
+
+if [ -z "$action_or_issue_id" ]; then
+  action_or_issue_id=create;
+fi
+
+if [ "$action_or_issue_id" = "create" ]; then
   jira_url="$jira_url/secure/CreateIssue!default.jspa";
+elif [ "$action_or_issue_id" = "dashboard" ]; then
+  jira_url="$jira_url/secure/Dashboard.jspa";
+elif [[ "$action_or_issue_id" =~ ^[A-Z]+-[0-9]+$ ]]; then
+  jira_url="$jira_url/browse/$action_or_issue_id";
+else
+  echo "Could not determine action";
+  exit 1;
 fi
 
 $open_cmd "$jira_url";
