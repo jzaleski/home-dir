1a2967e8a8752c854ee33a37f8998ce186639949
Jonathan W. Zaleski <JonathanZaleski@gmail.com>
2018-11-17 08:50:10 -0500

    Make `{my,p}sql` helpers fully cross-compatible

--- home/.home_dir/init/rc4/mysql.bash
+++ home/.home_dir/init/rc4/mysql.bash
@@ -4,7 +4,7 @@ pushd $HOME > /dev/null;
 for database_and_environment in `find . -maxdepth 1 -mindepth 1 -type f -name ".my.cnf-*" | sed -E 's/(\.\/)?\.my\.cnf-//'`; do
   database=`echo $database_and_environment | cut -d- -f1`;
   environment=`echo $database_and_environment | cut -d- -f2`;
-  alias mysql-$database-$environment="MYCNFFILE=\"$HOME/.my.cnf-$database_and_environment\" mysql";
+  alias mysql-$database-$environment="MYCNFFILE=\"$HOME/.my.cnf-$database_and_environment\" $HOME/bin/mysql.sh";
 done
 # return the previous directory
 popd > /dev/null;
--- home/.home_dir/init/rc4/mysql.fish
+++ home/.home_dir/init/rc4/mysql.fish
@@ -4,7 +4,7 @@ pushd $HOME > /dev/null;
 for database_and_environment in (find . -maxdepth 1 -mindepth 1 -type f -name ".my.cnf-*" | sed -E 's/(\.\/)?\.my\.cnf-//')
   set database (echo $database_and_environment | cut -d- -f1);
   set environment (echo $database_and_environment | cut -d- -f2);
-  alias mysql-$database-$environment="env MYCNFFILE=\"$HOME/.my.cnf-$database_and_environment\" mysql";
+  alias mysql-$database-$environment="env MYCNFFILE=\"$HOME/.my.cnf-$database_and_environment\" $HOME/bin/mysql.sh";
 end
 # return the previous directory
 popd > /dev/null;
