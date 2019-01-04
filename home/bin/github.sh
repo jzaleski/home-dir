67b8a684bf72f37b970649733f164f76e0005237
Jonathan W. Zaleski <JonathanZaleski@gmail.com>
2019-01-04 08:22:37 -0500

    Simplify the `github` helper

--- home/bin/github.sh
+++ home/bin/github.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -e;
+
 open_cmd=`\which open 2> /dev/null`;
 if [ -z "$open_cmd" ]; then
   open_cmd=`\which xdg-open 2> /dev/null`;
@@ -8,6 +10,11 @@ fi
 if [ -z "$open_cmd" ]; then
   echo "Could not locate the \"[xdg-]open\" binary";
   exit 1;
+fi
+
+repository=$1;
+if [ -n "$repository" ]; then
+  cd ${SOURCE_DIRECTORY-$HOME/src}/$repository;
 fi
 
 github_url=$GITHUB_URL;
@@ -32,13 +39,6 @@ fi
 https_prefix="https://github.com";
 if [[ ! "$github_url" =~ "$https_prefix" ]]; then
   github_url="$https_prefix/$github_url";
-fi
-
-argv0=$1;
-if [[ "$argv0" =~ ^[0-9]+$ ]]; then
-  github_url="$github_url/pull/$argv0";
-elif [ -n "$argv0" ]; then
-  github_url="$github_url/tree/$argv0";
 fi
 
 $open_cmd "$github_url";
