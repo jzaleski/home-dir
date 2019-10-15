c435d7669de9c96411aceab55ab438d8d4811df7
Jonathan W. Zaleski <JonathanZaleski@gmail.com>
2019-10-15 18:09:39 -0400

    Remove `java` helpers, they are now re-dundant

--- home/.home_dir/init/rc4/java.bash
+++
@@ -1,9 +0,0 @@
-library_java_home_dir=/Library/Java/Home;
-if [ -e $library_java_home_dir ] && [ -z "$JAVA_HOME" ]; then
-  export JAVA_HOME=$library_java_home_dir;
-fi
-
-java_bin_dir=$JAVA_HOME/bin;
-if [ -e $java_bin_dir ] && [[ ! "$PATH" =~ "$java_bin_dir" ]]; then
-  export PATH=$java_bin_dir:$PATH;
-fi
--- home/.home_dir/init/rc4/java.fish
+++
@@ -1,9 +0,0 @@
-set library_java_home_dir /Library/Java/Home;
-if begin; test -e $library_java_home_dir; and test -z "$JAVA_HOME"; end
-  set JAVA_HOME $library_java_home_dir;
-end
-
-set java_bin_dir $JAVA_HOME/bin;
-if begin; test -e $java_bin_dir; and not contains $java_bin_dir $PATH; end
-  set PATH $java_bin_dir $PATH;
-end
