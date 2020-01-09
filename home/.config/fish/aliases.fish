27c6a18bc9b7f50c8934f3418c42415cfea38647
Jonathan W. Zaleski <JonathanZaleski@gmail.com>
2020-01-09 16:23:25 -0500

    Clean-up fish-aliases

--- home/.config/fish/aliases.fish
+++ home/.config/fish/aliases.fish
@@ -4,23 +4,23 @@ alias edit='$EDITOR';
 alias find='~/lib/find.sh';
 alias gg='git grep';
 alias github='~/lib/github.sh';
-alias goal='env LIST_NAME="GOALS" ~/lib/list.py';
+alias goal='env LIST_NAME="GOALS" list-cli';
 alias htop='~/lib/htop.sh';
-alias icebox='env LIST_NAME="ICEBOX" ~/lib/list.py';
+alias icebox='env LIST_NAME="ICEBOX" list-cli';
 alias jira='env ~/lib/jira.sh';
 alias l='~/lib/ls.sh -CF';
 alias la='~/lib/ls.sh -la';
 alias ll='~/lib/ls.sh -l';
 alias ls='~/lib/ls.sh';
 alias mysql='~/lib/mysql.sh';
-alias note='env LIST_NAME="NOTES" ~/lib/list.py';
+alias note='env LIST_NAME="NOTES" list-cli';
 alias psql='~/lib/psql.sh';
-alias question='env LIST_NAME="QUESTIONS" ~/lib/list.py';
+alias question='env LIST_NAME="QUESTIONS" list-cli';
 alias readlink='~/lib/readlink.sh';
 alias screen='~/lib/screen.sh';
 alias setenv='source ~/lib/setenv.fish';
 alias tmux='~/lib/tmux.sh';
-alias todo='env LIST_NAME="TODO" ~/lib/list.py';
+alias todo='env LIST_NAME="TODO" list-cli';
 alias vi='vim';
 
 # The ability to navigate 10 directories up should be enough to cover most cases
