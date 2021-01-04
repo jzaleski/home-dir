9a128706ed9403489a3a00c281cc29e17eda3762
Jonathan W. Zaleski <JonathanZaleski@gmail.com>
2021-01-04 07:59:13 -0600

    Add support for parsing `mysql` connection-strings

--- home/lib/mysql.sh
+++ home/lib/mysql.sh
@@ -6,6 +6,12 @@ if [ -z "$mysql_cmd" ]; then
   exit 1;
 fi
 
+sed_cmd=$(which sed 2> /dev/null || echo -n);
+if [ -z "$sed_cmd" ]; then
+  echo "Could not locate \"sed\" binary";
+  exit 1;
+fi
+
 if [ -z "$MYCNFFILE" ]; then
   if [ -e "$PWD/.my.cnf" ]; then
     export MYCNFFILE="$PWD/.my.cnf";
@@ -14,8 +20,16 @@ if [ -z "$MYCNFFILE" ]; then
   fi
 fi
 
-if [ -n "$MYCNFFILE" ]; then
-  eval MYCNFFILE="" $mysql_cmd --defaults-file=$MYCNFFILE "$@";
+if [[ "$1" =~ "mysql://" ]]; then
+  IFS=":" read -a line_parts <<< $(echo $1 | $sed_cmd 's|mysql://||g; s|[@/]|:|g');
+  $mysql_cmd \
+    --user="${line_parts[0]}" \
+    --password="${line_parts[1]}" \
+    --host="${line_parts[2]}" \
+    --port=${line_parts[3]} \
+    "${line_parts[4]}";
+elif [ -n "$MYCNFFILE" ]; then
+  eval MYCNFFILE="" $mysql_cmd --defaults-file="$MYCNFFILE";
 else
   $mysql_cmd "$@";
 fi
