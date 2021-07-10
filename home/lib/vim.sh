50da1b2f9ea372459ce9d3ea7ae3d6d8738d817c
Jonathan W. Zaleski <JonathanZaleski@gmail.com>
2021-07-10 12:00:34 -0500

    Simplify `vim` wrapper-script (launching `gvim` or `nvim` can be done directly)

--- home/lib/vim.sh
+++ home/lib/vim.sh
@@ -1,16 +1,9 @@
 #!/usr/bin/env bash
 
-gvim_cmd=$(which gvim 2> /dev/null || echo -n);
-nvim_cmd=$(which nvim 2> /dev/null || echo -n);
 vim_cmd=$(which vim 2> /dev/null || echo -n);
-
-if [ -n "$gvim_cmd" ] && [ "$NO_GUI" != "true" ]; then
-  $gvim_cmd "$@";
-elif [ -n "$nvim_cmd" ]; then
-  $nvim_cmd "$@";
-elif [ -n "$vim_cmd" ]; then
-  $vim_cmd "$@";
-else
-  echo "Could not locate \"vim\" binary or equivalent";
+if [ -z "$vim_cmd" ]; then
+  echo "Could not locate \"vim\" binary";
   exit 1;
 fi
+
+$vim_cmd "$@";
