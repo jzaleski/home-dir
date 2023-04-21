c2ac9591ee8d55a62144a7822594ebdef3264e89
Jonathan W. Zaleski <JonathanZaleski@gmail.com>
2023-04-21 11:02:21 -0500

    Fix _[re]hash_ issue w/in `jenv` init-script

--- home/.home_dir/init/rc4/jenv.bash
+++ home/.home_dir/init/rc4/jenv.bash
@@ -11,15 +11,15 @@ fi
 if which jenv > /dev/null 2>&1; then
   noinit_file=$HOME/.noinit;
   if [ ! -e "$noinit_file" ] && [ ! -e "$HOME/.jenv/plugins/export" ]; then
-    $jenv_cmd sh-enable-plugin export;
+    jenv sh-enable-plugin export;
   fi
 
   if [ ! -e "$noinit_file" ] && [ ! -e "$HOME/.jenv/plugins/maven" ]; then
-    $jenv_cmd sh-enable-plugin maven;
+    jenv sh-enable-plugin maven;
   fi
 
   norehash_file=$HOME/.norehash;
   if [ ! -e $norehash_file ]; then
-    $jenv_cmd sh-rehash;
+    jenv sh-rehash;
   fi
 fi
