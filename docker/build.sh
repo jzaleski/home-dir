07d96d6d094cf7ae342c480a9eb79421c8d27b13
Jonathan W. Zaleski <JonathanZaleski@gmail.com>
2023-05-15 15:24:08 -0500

    Add support for a `$FORCE` flag when re-building to skip the cache (if applicable)

--- docker/build.sh
+++ docker/build.sh
@@ -4,7 +4,13 @@ set -e;
 
 source "$(dirname "$0")"/../.env;
 
+if [ "$FORCE" = "true" ]; then
+  NO_CACHE_FLAG="--no-cache";
+fi
+
 $DOCKER_CMD build \
   --build-arg USER \
   --tag "$IMAGE_NAME":latest \
+  $NO_CACHE_FLAG \
+  --pull \
   "$(dirname "$0")"/..;
