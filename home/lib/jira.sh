64e911b3ded34df69fbb7ea3d608d5fffe3064f7
Jonathan W. Zaleski <JonathanZaleski@gmail.com>
2024-05-10 15:32:24 -0400

    fix: Allow numbers in backlogs / board-ids for _Jira_ helper

--- home/lib/jira.sh
+++ home/lib/jira.sh
@@ -47,7 +47,7 @@ git_branch=$($git_cmd rev-parse --abbrev-ref HEAD 2> /dev/null || echo -n);
 if \
   [ -z "$action_or_issue_id" ] && \
   [ -n "$git_branch" ] && \
-  [[ "$git_branch" =~ ^[A-Z]+-[0-9]+$ ]];
+  [[ "$git_branch" =~ ^[A-Z0-9]+-[0-9]+$ ]];
 then
   action_or_issue_id=$git_branch;
 fi
@@ -60,7 +60,7 @@ if [ "$action_or_issue_id" = "create" ]; then
   jira_url="$jira_url/secure/CreateIssue!default.jspa";
 elif [ "$action_or_issue_id" = "dashboard" ]; then
   jira_url="$jira_url/secure/Dashboard.jspa";
-elif [[ "$action_or_issue_id" =~ ^[A-Z]+-[0-9]+$ ]]; then
+elif [[ "$action_or_issue_id" =~ ^[A-Z0-9]+-[0-9]+$ ]]; then
   jira_url="$jira_url/browse/$action_or_issue_id";
 else
   echo "Could not determine action";
