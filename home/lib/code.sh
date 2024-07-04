65b505639310c2c74206bd1253c8891265fda05e
Jonathan W. Zaleski <JonathanZaleski@gmail.com>
2024-07-04 11:41:50 -0400

    fix: Add support for `code` on _Linux_ as well

--- home/lib/code.sh
+++ home/lib/code.sh
@@ -2,7 +2,12 @@
 
 set -e;
 
-code_cmd="/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code";
+if [ $(uname) = "Darwin" ]; then
+  code_cmd="/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code";
+else
+  code_cmd="/usr/bin/code";
+fi
+
 if [ ! -e "$code_cmd" ]; then
   echo "Could not locate the \"code\" binary";
   exit 1;
