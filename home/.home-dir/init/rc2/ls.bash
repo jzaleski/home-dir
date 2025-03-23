607a1b5ae34b88f72d2cbc42acb6a83001d13653
Jonathan W. Zaleski <JonathanZaleski@gmail.com>
2025-03-23 09:44:38 -0400

    fix: Move `LS{,_}COLORS` configuration up to the shell-init process

---
+++ home/.home-dir/init/rc2/ls.bash
@@ -0,0 +1,7 @@
+unset LSCOLORS LS_COLORS;
+
+if [ $(uname) = "Darwin" ]; then
+  export LSCOLORS="ExFxBxDxCxegedabagacad";
+else
+  export LS_COLORS="di=1;36:ln=35:so=32:pi=33:ex=31:bd=34;46:cd=34;43:su=30;41:sg=30;46:tw=30;42:ow=30;43";
+fi
--- home/lib/ls.sh
+++ home/lib/ls.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
 
-LS_COLORS=${LS_COLORS:-"di=1;36:ln=35:so=32:pi=33:ex=31:bd=34;46:cd=34;43:su=30;41:sg=30;46:tw=30;42:ow=30;43"};
-
-\ls --color "$@";
+\ls --color=auto "$@";
